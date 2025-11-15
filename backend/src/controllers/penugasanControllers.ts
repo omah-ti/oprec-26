2061097956443e6e9c412efada0835541bef6945
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-11-15 12:41:19 +0700

    fix(time): tes

--- backend/src/controllers/penugasanControllers.ts
+++ backend/src/controllers/penugasanControllers.ts
@@ -6,8 +6,8 @@ import { IGetRequestWithUser } from "../types/getUserRequest";
 
 // Pengumpulan penugasan dibuka: 15 November 2025, 12:00 PM
 // Pengumpulan penugasan ditutup: 21 November 2025, 12:00 PM
-const SUBMISSION_OPEN_DATE = new Date(2025, 10, 15, 5, 0); // November 15, 2025, 12:00 PM
-const SUBMISSION_CLOSE_DATE = new Date(2025, 10, 21, 5, 0); // November 21, 2025, 12:00 PM
+const SUBMISSION_OPEN_DATE = new Date(Date.UTC(2025, 10, 15, 5, 0, 0)); // 15 Nov 2025, 12:00 WIB
+const SUBMISSION_CLOSE_DATE = new Date(Date.UTC(2025, 10, 21, 5, 0, 0)); // 21 Nov 2025, 12:00 WIB
 
 const checkSubmissionPeriod = (): { isOpen: boolean; message?: string } => {
     const now = new Date();
